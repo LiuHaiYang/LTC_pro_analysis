804935eb17d8385988a6f8a620846fd7cc5a4ab7
LiuHaiYang <843800695@qq.com>
2017-08-07 16:09:19 +0800

    ipython

---
+++ ipython/ipython_log.py
@@ -0,0 +1,56 @@
+# IPython log file
+
+import pandas as pd
+get_ipython().magic('matplotlib inline')
+import pandas as pd
+import numpy as np
+import os
+import matplotlib.pyplot as pls
+img = plt.imread('G:\\个人资料\\图库\\桌面\\112.jpg')
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+img = plt.imread('G:\\个人资料\\图库\\桌面\\112.jpg')
+img = plt.imread('G:\\个人资料\\图库\\桌面\\1111.png')
+imshow(img)
+img
+get_ipython().magic('matplotlib inline')
+import pandas as pd
+import image
+get_ipython().magic('matplotlib inline')
+import pandas as pd
+import image
+imshow(img)
+img.show()
+img = image.open('G:\\个人资料\\图库\\桌面\\1111.png')
+img = plt.imread('G:\\个人资料\\图库\\桌面\\1111.png')
+plt.imshow(img)
+plt(randn(1000).cumsum())
+get_ipython().magic('matplotlib inline')
+import pandas as pd
+import image
+from numpy.random import randn
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+img = plt.imread('G:\\个人资料\\图库\\桌面\\1111.png')
+plt.imshow(img)
+plt(randn(1000).cumsum())
+a = plt(randn(1000).cumsum())
+a
+get_ipython().magic('logstart')
+2**27
+get_ipython().magic('dhist')
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import time
+img = plt.imread('G:\\个人资料\\图库\\桌面\\1111.png')
+plt.imshow(img)
+get_ipython().magic('logstart')
+2**27
+get_ipython().magic('dhist')
+x = 'foobar'
+y = 'foo'
+get_ipython().magic('timeit x.startswith(y)')
+get_ipython().magic('timeit x[:3] ==y')
